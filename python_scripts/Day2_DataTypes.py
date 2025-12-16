ddba4f41a163bde860e2e5cf82e0485323fbf1a9
cara-machi <maymayapt1@gmail.com>
2025-12-16 16:42:45 +0800

    Add Day 2 DataTypes python script

---
+++ python_scripts/Day2_DataTypes.py
@@ -0,0 +1,11 @@
+#Day2_DataTypes.py
+#Task: Experiment with different data types 
+
+a=5
+b=3.9
+c="May"
+d=False
+
+print(type(a),type(b),type(c),type(d))
+print("Convert flow into int",int(b))
+print("Convert int into string",str(a))
