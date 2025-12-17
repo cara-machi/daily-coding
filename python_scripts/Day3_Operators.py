ecca93907f2c82ca15734da26d766cd8b3410c1a
cara-machi <maymayapt1@gmail.com>
2025-12-17 11:17:14 +0800

    Practise applying operators

---
+++ python_scripts/Day3_Operators.py
@@ -0,0 +1,19 @@
+#Day3_Operators.py
+#Tasks: Practise Operators
+
+x=101
+y=24
+
+print("Add:",x+y)
+print("Subtract:",x-y)
+print("Mutiply:",x*y)
+print("Divide:", x/y)
+print("Floor Divide:",x//y)
+print("Remainder:",x%y)
+print("Power:",x**y)
+
+print("x>y?",x>y)
+print("x==y?",x==y)
+
+# when running print("x>y,x.y),the result shows x>y? True
+# pylance error traceback: inside the print parentheses, "," should not be left out between ""and non ""
